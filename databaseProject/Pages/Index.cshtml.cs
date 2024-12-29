05ec8b6be7e41345cdf33a3c1b6c6be0dcbe0d04
MrAAAswat <a.aswatliu@gmail.com>
2024-12-30 01:24:47 +0800

    Update Index.cshtml.cs

--- databaseProject/Pages/Index.cshtml.cs
+++ databaseProject/Pages/Index.cshtml.cs
@@ -12,9 +12,37 @@ namespace databaseProject.Pages
             _webHostEnvironment = webHostEnvironment;
         }
 
+        // Property to hold the categories displayed on the home page
+        public List<Category> Categories { get; set; }
+
         public void OnGet()
         {
-
+            // Initialize the categories for display
+            Categories = new List<Category>
+            {
+                new Category
+                {
+                    Name = "Fruits",
+                    ImagePath = "/images/fruits.jpg"
+                },
+                new Category
+                {
+                    Name = "Vegetables",
+                    ImagePath = "/images/vegetables.jpg"
+                },
+                new Category
+                {
+                    Name = "Snacks",
+                    ImagePath = "/images/snacks.jpg"
+                }
+            };
         }
+    }
+
+    // Class to represent a category
+    public class Category
+    {
+        public string Name { get; set; }        // Category name (e.g., "Fruits")
+        public string ImagePath { get; set; }  // Path to the category image
     }
 }
