ec6224df7bffbe49577b3608f7835b2b3347d757
fiersungod <fier.sun.god@gmail.com>
2024-12-30 22:12:44 +0800

    modify Page

---
+++ databaseProject/Pages/memberPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace databaseProject.Pages
+{
+    public class memberPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
