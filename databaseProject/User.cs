bd1b85aae6656216c880cd305bfba7778e9fb988
fiersungod <fier.sun.god@gmail.com>
2025-01-02 14:21:15 +0800

    e04

--- databaseProject/User.cs
+++ databaseProject/User.cs
@@ -4,8 +4,8 @@ namespace databaseProject
 {
     public static class User
     {
-        public static string UserId { get; set; } = "mem";
-        public static string CartId { get; set; } = "47efsg23e";
-        public static string MemberType { get; set; } = "Member";
+        public static string UserId { get; set; } = String.Empty;
+        public static string CartId { get; set; } = String.Empty;
+        public static string MemberType { get; set; } = String.Empty;
     }
 }
